bdb9f70b0be819e2f149b52b61d512c434ce94d7
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-17 21:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+let scrollContent = document.getElementById("infi-list");
+
+let nextEle = 1;
+
+function renderElement() {
+
+	for (let index = 0; index < 20; index++) {
+		let element = document.createElement('li');
+		element.innerHTML = 'Item' + nextEle++;
+		scrollContent.appendChild(element);
+	}
+}
+
+renderElement()
+
+scrollContent.addEventListener("scroll",function(){
+	if (scrollContent.scrollTop + scrollContent.clientHeight >= scrollContent.scrollHeight) {
+		renderElement();
+	}
+})
 
